7c586e6294e54adf0b496abb0e69f52762d96795
FlavioCesarGS <inquicidor@outlook.es>
2023-07-31 15:31:18 -0600

    terminado

---
+++ seccion 8/funcion flecha.js
@@ -0,0 +1,18 @@
+let sumar = function(a,b,c){
+    console.log(arguments[0]);
+    console.log(arguments[1])
+    console.log(arguments[2])
+    return a+b+c 
+
+};
+
+
+resultado = sumar(12,10,7);
+console.log(resultado)
+
+/*FUNCION TIPO FLECHA
+
+const SumarFuncionTipoFlecha = (a,b) => a + b;
+resultado = SumarFuncionTipoFlecha(3,5);
+console.log(resultado);
+*/
