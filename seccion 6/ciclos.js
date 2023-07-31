54252a33f11dd51c6b711b891c5aee6c56e06655
FlavioCesarGS <inquicidor@outlook.es>
2023-07-31 13:19:26 -0600

    ...

--- seccion 6/ciclos.js
+++ seccion 6/ciclos.js
@@ -1,15 +1,16 @@
 let contador = 0;
-/*
+
 while( contador <3 ){
     console.log(contador);
     contador++;
+    break;
 }
 console.log("Fin del ciclo while");
 
-*/
 
+/*
 do{
     console.log(contador);
     contador++;
 } while(contador<3);
-console.log("Fin ciclo do while");
+console.log("Fin ciclo do while");*/
